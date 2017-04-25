c14d5e8ec9973dab714cf98958e9582f81d5998a
TheMuffinMan5930 <cjlonergan2@yahoo.com>
2017-04-25 19:22:11 -0400

    Update Attacking System.py

--- Attacking System.py
+++ Attacking System.py
@@ -1,14 +1,24 @@
 import gspread
+import "https://github.com/TheMuffinMan5930/NarutoBattle/blob/master/Chat.py"
 Jitsu_Player = sh.worksheet(Player_Name + "- Jitsus")
 i = 1
 while True:
-  exec("Jit" + i + " = worksheet.acell(" + i.string.ascii_uppercase.index(i - 1) + "2)")
+  exec("Jit" + i + " = worksheet.acell(" + i.string.ascii_uppercase.index(i - 1).value + "2)")
   i += 1
 confirmation_of_jitsus = input("Is it correct that you have #@, #@, #@, #@, #@, #@, #@ as your ttjitsus? Y/N").format(Jit1, Jit2, Jit3, Jit4, Jit5, Jit6, Jit7)
 try:
   if confirmation_of_jitsus == Y
-    BattleReady()
+    worksheet.update.acell("Ready")
   elif confirmation_of_jitsus == N
     QuitGame() # Def this aswell 
 except:
     print("Error")
+# turn progressing code here
+# this is just the code I CAN do.
+""" def Attack(Jitsu)
+  Attack_Power ......  # I will do this soon
+while your_turn = True 
+  jitsu_using = input("What jitsu would you like to use? If you need a reminder of your jitsus, type remind me")
+  if jitsu_using = Jit1
+    Attack(Jit1)
+    
