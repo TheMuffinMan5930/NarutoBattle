d76e3b5aac23916565eef02146c1be6f944200b1
Chris Lonergan <chrislonergan@Cheryls-MacBook-Pro.local>
2017-04-27 18:04:32 -0400

    Still needs attack attributes and correct coordinates

--- Attacking System.py
+++ Attacking System.py
@@ -6,19 +6,20 @@ while True:
   exec("Jit" + i + " = worksheet.acell(" + i.string.ascii_uppercase.index(i - 1).value + "2)")
   i += 1
 confirmation_of_jitsus = input("Is it correct that you have {}, {}, {}, {}, {}, {}, {} as your ttjitsus? Y/N".format(Jit1, Jit2, Jit3, Jit4, Jit5, Jit6, Jit7)
-try:)
-  if confirmation_of_jitsus == Y
+if confirmation_of_jitsus == Y
     worksheet.update.acell("Ready")
-  elif confirmation_of_jitsus == N
+elif confirmation_of_jitsus == N
     QuitGame() # Def this aswell 
 except:
     print("Error")
 # turn progressing code here
 # this is just the code I CAN do.
-""" def Attack(Jitsu)
-  Attack_Power ......  # I will do this soon
-while your_turn = True 
-  jitsu_using = input("What jitsu would you like to use? If you need a reminder of your jitsus, type remind me")
-  if jitsu_using = Jit1
-    Attack(Jit1)
+class Attack(object)
+	def __init__(self)
+		self.range = wsJitsus.cell( , ,.val)#blah, Blah plus syntax check for 19 - 22ish
+		self.damage
+		self. #other attributes
+		self.
+	def __str__(self)
+		return(Your Jitsu has {}d).format(Jitsu)
     
