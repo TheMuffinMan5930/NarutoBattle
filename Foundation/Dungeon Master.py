cd174c2bae942ac85ebbaa5895fcefdccaeacd2e
Chris Lonergan <chrislonergan@Cheryls-MacBook-Pro.local>
2017-04-28 18:18:07 -0400

    Blap

--- Foundation/Dungeon Master.py
+++ Foundation/Dungeon Master.py
@@ -1,14 +1,14 @@
 if Player_Name = DM or DungeonMaster
   def TakeHP = raw_input("Which player would you like to take HP from?")
   if player = Player1
-    Player1HP -= 1 //is this how you do it? NOAH!
+    Player1HP -= 1 
   elif player = Player2
-    Player2HP -= 1 // And so on and so on...
+    Player2HP -= 1 
   else:
     print("Not a player")
 
-  def SpawnEnemy(HP, Atk, Per, Ag, Def, Cha, Location) // double check this is the name of all of the stats
-    print("An enemy has been spawned at $@, with #@ Health, #@ Attack, #@ Perception, #@ Agility, #@ Defense, and #@ Chakra!") (Location, HP, Atk, Per, Ag, Def, Cha)
+  def SpawnEnemy(HP, Atk, Per, Ag, Def, Cha, Location) #double check this is the name of all of the stats
+    shEnemies.cell(1, 0 + i,"An enemy has been spawned at {}d, with {}d Health, {}d Attack, {}d Perception, {}d Agility, {}d@ Defense, and {}d Chakra!".format(Location, HP, Atk, Per, Ag, Def, Cha))
   def Effect_Give //effect or affect?
     Effect_Given = raw_input("What affect would you like to give?")
       if Effect_Given = //Effect1 goes here
